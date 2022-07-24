1a4b32a49c0b3a8380b164fdac51aa760c1b52ba
Harshit Prajapati <prajapati.harshit.73@gmail.com>
2022-07-24 23:09:30 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,36 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <h1> 1a.l </h1>
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         %{
+#include<stdio.h>
+int v=0,op=0,id=0,flag=0;
+%}
+
+%%
+[a-zA-Z]+[0-9A-Za-z]* {id++;}
+[0-9]+ {id++;}
+[\+\-\*/\=] {op++;}
+"(" {v++;}
+")" {v--;}
+";" {flag=1;}
+.|\n {return 0;}
+%%
+
+int main()
+{
+        printf("Enter the expression:");
+        yylex();
+        if((op+1)==id && v==0 && flag==0)
+        {
+                printf("\n Expression is Valid\n");
+                printf("No of identifier = %d \n No of Operators = %d \n",id,op);
+        }
+        else
+                printf("\n Expression is Invalid\n");
+return 0;
+}
         </p>
       </main>
 
